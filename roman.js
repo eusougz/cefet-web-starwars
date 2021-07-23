2f1f918f830157b76fb466e94426ad49a0ae7b2a
eusougz <guilhermegms209@gmail.com>
2021-07-23 00:03:16 -0300

    Atividade realizada

---
+++ roman.js
@@ -0,0 +1,16 @@
+export function numero2romano(numero) {
+  const numerosRomanos = {
+    1: 'I     ',
+    2: 'II    ',
+    3: 'III   ',
+    4: 'IV    ',
+    5: 'V     ',
+    6: 'VI    ',
+    7: 'VII   ',
+    8: 'VIII  ',
+    9: 'IX    ',
+    10: 'X    '
+  }
+
+  return numerosRomanos[numero]
+}
--- starwars.js
+++ starwars.js
@@ -4,4 +4,44 @@
 //  - Pegar a lista de filmes (AJAX) e preencher no HTML
 //  - Quando um filme for clicado, exibir sua introdução
 
+import { play } from './music.js';
+import { numero2romano } from './roman.js';
+import { restartAnimation } from './restart-animation.js';
+
 const API_ENDPOINT = 'https://swapi.dev/api'
+
+play({
+  audioUrl: 'audio/tema-sw.mp3',
+  coverImageUrl: 'imgs/logo.svg',
+  title: 'Intro',
+  artist: 'John Williams'
+}, document.body);
+
+const resposta = await fetch(`${API_ENDPOINT}/films`);
+const { results } = await resposta.json();
+const filmes = results;
+console.log(filmes);
+
+
+const listaFilmesEl = document.querySelector('#filmes ul');
+const preIntroEl = document.querySelector('pre.introducao');
+
+listaFilmesEl.innerHTML = '';
+let numeroFilme = 1;
+filmes.forEach(filme => {
+  let novoFilmeEl = document.createElement('li');
+  novoFilmeEl.innerHTML = `<li>Episode ${numero2romano(numeroFilme)} - ${filme.title}</li>`;
+  listaFilmesEl.appendChild(novoFilmeEl);
+  numeroFilme++;
+
+  novoFilmeEl.addEventListener('click', () => {
+    preIntroEl.innerHTML = `
+      Episode ${numero2romano(numeroFilme)}
+      ${filme.title}
+
+      ${filme.opening_crawl}
+    `;
+
+    restartAnimation(preIntroEl)
+  })
+});
